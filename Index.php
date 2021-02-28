b69c0eab7209a49979484ce073872225ef5068bc
ridhoafriyando <78814296+ridhoafriyando@users.noreply.github.com>
2021-02-28 22:36:00 +0700

    Praktikum 03
    Membuat class User, MahasiswaBaru, Pegawai dan Dosen

--- Index.php
+++ Index.php
@@ -1,33 +1,33 @@
 <?php
- 
-include('Mahasiswa.php');
- 
-$mahasiswa_pertama = new Mahasiswa();
- 
-$mahasiswa_pertama->nim = "H1101191066";
-$mahasiswa_pertama->nama = "Ridho arjumal afriyando";
-$mahasiswa_pertama->tanggal_lahir = "24 september 1999":
-$mahasiswa_pertama->jenis_kelamin = "laki-laki":
+    require_once ("Mahasiswa.php");
+    require_once ("MahasiswaBaru.php");
 
-$mahasiswa_kedua = new Mahasiswa ();
 
-$mahasiswa_kedua->nim = "H1101191034";
-$mahasiswa_kedua->nama = "Muhammad Fadillah";
-$mahasiswa_kedua->tanggal_lahir = "3 januari 2001";
-$mahasiswa_kedua->jenis_kelamin = "laki-laki"
+    $ridho = new Mahasiswa("H1101191066", "Ridho Arjumal Afriyando", "1999-09-24", "laki-laki");
+    $azril = new MahasiswaBaru("H1101011065", "M. Azril Saktia", "2002-01-21", "laki-laki", "111065");
 
-$mahasiswa_ketiga = new Mahasiswa();
+?>
 
-$mahasiswa_ketiga->nim = ""
-$mahasiswa_ketiga->nama = "Muhammad Pramudia Irvan"
-$mahasiswa_ketiga->tanggal_lahir = " 5 mei 2001"
-$mahasiswa_ketiga->jenis_kelamin = "laki-laki"
-
-$mahasiswa_pertama->tampilkanUmur();
-$mahasiswa_pertama->tampilkanAngkatan();
-
-$mahasiswa_kedua->tampilkanUmur();
-$mahasiswa_kedua->tampilkanAngkatan():
-
-$mahasiswa_ketiga->tampilkanUmur();
-$mahasiswa_ketiga->tampilkanAngkatan();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+    <?php echo "<p>Hello World</p>"; ?>
+    
+    <?php
+        $ridho->tampilkanAngkatan();
+        echo "<br>";
+        $azril->tampilkanAngkatan();
+        echo "<br>";
+        echo "<br>";
+        $ridho->tampilkanUmur();
+        echo "<br>";
+        $azril->tampilkanUmur();
+    ?>
+</body>
+</html>
